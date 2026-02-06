47e79c3b288113e23c104de49c9882f538281a1c
Yunus Emre Deligöz <ye@deligoz.me>
2026-02-06 14:08:11 +0300

    refactor(AssertionParserResult): remove assertions, segments, expects properties (only executableCode + resultComments remain)

--- src/Assertion/AssertionParserResult.php
+++ src/Assertion/AssertionParserResult.php
@@ -7,16 +7,10 @@ namespace TestFlowLabs\DocTest\Assertion;
 final readonly class AssertionParserResult
 {
     /**
-     * @param array<Assertion> $assertions
-     * @param array<CodeSegment> $segments
-     * @param array<ExpectAssertion> $expects
      * @param array<ResultCommentAssertion> $resultComments
      */
     public function __construct(
-        public array $assertions,
         public string $executableCode,
-        public array $segments,
-        public array $expects,
         public array $resultComments = [],
     ) {}
 }
