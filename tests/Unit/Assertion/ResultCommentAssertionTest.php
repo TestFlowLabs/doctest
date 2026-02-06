6fbe941d1d11926a025e24c91555935231b34860
Yunus Emre Deligöz <ye@deligoz.me>
2026-02-06 12:38:29 +0300

    test(ResultCommentAssertionTest): add unit tests for result comment assertion (interface, type, expression, expected value, line)

---
+++ tests/Unit/Assertion/ResultCommentAssertionTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TestFlowLabs\DocTest\Tests\Unit\Assertion;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use TestFlowLabs\DocTest\Assertion\Assertion;
+use TestFlowLabs\DocTest\Assertion\ResultCommentAssertion;
+
+final class ResultCommentAssertionTest extends TestCase
+{
+    #[Test]
+    public function implements_assertion_interface(): void
+    {
+        $assertion = new ResultCommentAssertion('$x', 'true', 1);
+
+        $this->assertInstanceOf(Assertion::class, $assertion);
+    }
+
+    #[Test]
+    public function returns_result_comment_type(): void
+    {
+        $assertion = new ResultCommentAssertion('$x', 'true', 1);
+
+        $this->assertSame('result_comment', $assertion->type());
+    }
+
+    #[Test]
+    public function stores_expression(): void
+    {
+        $assertion = new ResultCommentAssertion('$state->matches(\'green\')', 'true', 5);
+
+        $this->assertSame('$state->matches(\'green\')', $assertion->expression);
+    }
+
+    #[Test]
+    public function stores_expected_value(): void
+    {
+        $assertion = new ResultCommentAssertion('$x', '42', 3);
+
+        $this->assertSame('42', $assertion->expectedValue);
+    }
+
+    #[Test]
+    public function stores_line_number(): void
+    {
+        $assertion = new ResultCommentAssertion('$x', 'true', 10);
+
+        $this->assertSame(10, $assertion->line());
+    }
+}
