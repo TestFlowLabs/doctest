bee1df0d1e0c121dc5a477efa283cd269e2d26b3
Yunus Emre Deligöz <ye@deligoz.me>
2026-02-06 10:20:55 +0300

    refactor(CodeGeneratorGroupTest): use first-class callable for unlink (Rector FunctionFirstClassCallableRector)

--- tests/Unit/Executor/CodeGeneratorGroupTest.php
+++ tests/Unit/Executor/CodeGeneratorGroupTest.php
@@ -28,7 +28,7 @@ final class CodeGeneratorGroupTest extends TestCase
         $dir = sys_get_temp_dir() . '/doctest';
 
         if (is_dir($dir)) {
-            array_map('unlink', glob($dir . '/*.php') ?: []);
+            array_map(unlink(...), glob($dir . '/*.php') ?: []);
         }
     }
 
