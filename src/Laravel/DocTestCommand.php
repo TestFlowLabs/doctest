a0be1e1b90ecbfbaf3dd701db1136a5ed7e226a8
Yunus Emre Deligöz <ye@deligoz.me>
2026-02-06 09:22:52 +0300

    feat(DocTestCommand): add execute method wiring DocTest to Symfony Console (integration)

--- src/Laravel/DocTestCommand.php
+++ src/Laravel/DocTestCommand.php
@@ -6,7 +6,12 @@ namespace TestFlowLabs\DocTest\Laravel;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\StreamOutput;
+use TestFlowLabs\DocTest\Config\DocTestConfig;
+use TestFlowLabs\DocTest\DocTest;
 
 final class DocTestCommand extends Command
 {
@@ -18,5 +23,30 @@ final class DocTestCommand extends Command
             ->addArgument('files', InputArgument::IS_ARRAY, 'Markdown files to test')
             ->addOption('filter', 'f', InputOption::VALUE_REQUIRED, 'Filter blocks by name pattern')
             ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Show what would be executed without running');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        /** @var array<string> $files */
+        $files = $input->getArgument('files');
+
+        $baseConfig = DocTestConfig::load();
+        $filter = $input->getOption('filter');
+
+        $config = new DocTestConfig(
+            paths: $files !== [] ? $files : $baseConfig->paths,
+            exclude: $files !== [] ? [] : $baseConfig->exclude,
+            timeout: $baseConfig->timeout,
+            memoryLimit: $baseConfig->memoryLimit,
+            stopOnFailure: $baseConfig->stopOnFailure,
+            dryRun: $input->getOption('dry-run') === true || $baseConfig->dryRun,
+            filter: is_string($filter) ? $filter : $baseConfig->filter,
+            verbosity: $baseConfig->verbosity,
+        );
+
+        $stream = $output instanceof StreamOutput ? $output->getStream() : null;
+        $doctest = new DocTest($config, $stream);
+
+        return $doctest->run();
     }
 }
