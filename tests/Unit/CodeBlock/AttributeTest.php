ee0574783abbe4b73c64ff76cad4aa80a17e39b0
Yunus Emre Deligöz <ye@deligoz.me>
2026-02-06 09:11:21 +0300

    refactor(AttributeTest): remove redundant enum value tests, keep from/tryFrom behavior tests

---
+++ tests/Unit/CodeBlock/AttributeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TestFlowLabs\DocTest\Tests\Unit\CodeBlock;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use TestFlowLabs\DocTest\CodeBlock\Attribute;
+
+final class AttributeTest extends TestCase
+{
+    #[Test]
+    public function from_valid_string_returns_case(): void
+    {
+        $this->assertSame(Attribute::Ignore, Attribute::from('ignore'));
+        $this->assertSame(Attribute::NoRun, Attribute::from('no_run'));
+        $this->assertSame(Attribute::Throws, Attribute::from('throws'));
+        $this->assertSame(Attribute::ParseError, Attribute::from('parse_error'));
+        $this->assertSame(Attribute::Setup, Attribute::from('setup'));
+        $this->assertSame(Attribute::Teardown, Attribute::from('teardown'));
+    }
+
+    #[Test]
+    public function try_from_invalid_string_returns_null(): void
+    {
+        $this->assertNull(Attribute::tryFrom('invalid'));
+        $this->assertNull(Attribute::tryFrom(''));
+    }
+}
