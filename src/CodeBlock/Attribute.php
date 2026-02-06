df0152424e93a6b579aa93fa9ce3c00ff2ebfe4d
Yunus Emre Deligöz <ye@deligoz.me>
2026-02-06 09:42:21 +0300

    feat(Attribute): add code block attribute enum for ignore, no_run, throws, etc. (block classification)

---
+++ src/CodeBlock/Attribute.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TestFlowLabs\DocTest\CodeBlock;
+
+enum Attribute: string
+{
+    case Ignore = 'ignore';
+    case NoRun = 'no_run';
+    case Throws = 'throws';
+    case ParseError = 'parse_error';
+    case Setup = 'setup';
+    case Teardown = 'teardown';
+}
