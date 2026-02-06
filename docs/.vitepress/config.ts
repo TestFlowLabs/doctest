986b5ce283e22630e41f4837f468a0a848afe9b6
Yunus Emre Deligöz <ye@deligoz.me>
2026-02-06 18:32:28 +0300

    docs(config.ts): add Writing Testable Docs to Guide sidebar section

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
           { text: 'Getting Started', link: '/guide/getting-started' },
           { text: 'Installation', link: '/guide/installation' },
           { text: 'How It Works', link: '/guide/how-it-works' },
+          { text: 'Writing Testable Docs', link: '/guide/writing-testable-docs' },
         ],
       },
       {
